09117ba036dce2b520372aca8a99ac690f716409
Scott Morgan <scott@adligo.com>
2023-04-30 17:08:54 -0500

    Fixing missing settings file, and other cleanup

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+
+rootProject.name = "jse-core.adligo.org"
+include(
+"bytes.adligo.org", "bytes_gwt_examples.adligo.org", "bytes_tests.adligo.org",
+"collections.adligo.org","collections_gwt_examples.adligo.org","collections_tests.adligo.org",
+"ctx.adligo.org", "ctx_gwt_examples.adligo.org", "ctx_tests.adligo.org",
+"i_bytes.adligo.org",
+"i_collections.adligo.org",
+"i_ctx.adligo.org","i_ctx4jse.adligo.org",
+"i_pipe.adligo.org","i_tests4j.adligo.org",
+"i_threads.adligo.org","i_threads4jse.adligo.org",
+"mockito_ext.adligo.org",
+"pipe.adligo.org","pipe_tests.adligo.org",
+"ten.adligo.org", "ten_gwt_examples.adligo.org", "ten_tests.adligo.org",
+"tests4j.adligo.org","tests4j4jj.adligo.org", "tests4j4jj_tests.adligo.org",
+"tests4j_4mockito.adligo.org", "threads.adligo.org",)
